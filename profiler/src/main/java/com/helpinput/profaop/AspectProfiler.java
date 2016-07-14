0106c7f65fddd39aaf735cd49542408ce525cc33
niaoge <78493244@qq.com>
2016-07-14 17:14:26 +0800

    lic

--- profiler/src/main/java/com/helpinput/profaop/AspectProfiler.java
+++ profiler/src/main/java/com/helpinput/profaop/AspectProfiler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.helpinput.profaop;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -10,6 +25,12 @@ import org.slf4j.LoggerFactory;
 
 import com.helpinput.profile.Monitor;
 
+/**
+ * The Class AspectProfiler.
+ *
+ * @author niaoge(78493244@qq.com , niaoge@gmail.com)
+ * @version $Id: AspectProfiler.java, v 0.1 2016-7-14 17:13:46 niaoge Exp $$
+ */
 @Aspect
 public class AspectProfiler {
     final static Logger logger = LoggerFactory.getLogger(AspectProfiler.class);
@@ -22,6 +43,9 @@ public class AspectProfiler {
     void notAspectJ() {
     }
     
+    /**
+     * Not profiler.
+     */
     @Pointcut("!within(com.helpinput.profile..*)")
     void notProfiler() {
     }
